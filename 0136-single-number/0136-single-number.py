a19bafcf4af6374cfc22ddbaca17316cc7794a2f
harshithayannam <68652407+harshithayannam@users.noreply.github.com>
2023-10-11 16:38:03 -0700

    Time: 115 ms (92.31%), Space: 19.1 MB (16.69%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,13 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        new=[]
-        dup=[]
+        n=0
         for i in nums:
-            if i not in new:
-                new.append(i)
-            else:
-                dup.append(i)
-        for j in dup:
-            new.remove(j)
+            n^=i
+        return n
         
-        return new[0]
+        
+#         new=[]
+#         dup=[]
+#         for i in nums:
+#             if i not in new:
+#                 new.append(i)
+#             else:
+#                 dup.append(i)
+#         for j in dup:
+#             new.remove(j)
+        
+#         return new[0]
